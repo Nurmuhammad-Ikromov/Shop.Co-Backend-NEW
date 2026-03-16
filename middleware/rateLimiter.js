84e2b1d501b1fcad7b8b9593c796146ffe7484e8
Nurmuhammad-Ikromov <nurmuhammadikromov58@gmail.com>
2026-03-16 12:05:13 +0500

    change rate

--- middleware/rateLimiter.js
+++ middleware/rateLimiter.js
@@ -2,7 +2,7 @@ import rateLimit from "express-rate-limit";
 
 export const limiter = rateLimit({
   windowMs: 60 * 1000,
-  max: 20,
+  max: 60,
   standardHeaders: true,
   legacyHeaders: false,
   message: { error: "So'rovlar soni oshib ketdi. 1 daqiqadan so'ng qayta urinib ko'ring." },
